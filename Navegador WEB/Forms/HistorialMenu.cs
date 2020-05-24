8a46a0d9e3ff97189e1ff4b907f5d4011ba72287
DanielGCh1 <dgchaarturo@gmail.com>
2020-05-24 13:08:47 -0600

    Falta, cache y evitar multiples descargas

---
+++ Navegador WEB/Forms/HistorialMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavegadorWEB.Forms
+{
+    public partial class HistorialMenu : Form
+    {
+        public HistorialMenu()
+        {
+            InitializeComponent();
+            Mutex mutex = new Mutex();
+            mutex.WaitOne();
+            Thread.Sleep(50);
+            CargarHistorial();
+            Thread.Sleep(50);
+            mutex.ReleaseMutex();
+        }
+
+        private void ButtonBorrarHistorial_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("¿Desea borrar el historial de la aplicación?", "Confirmacion", MessageBoxButtons.YesNoCancel);
+            if (resultado == DialogResult.Yes)
+            {
+                Mutex mutex = new Mutex();
+                mutex.WaitOne();
+                Thread.Sleep(50);
+                Contenedor.ListaHistorial.Clear();
+                Thread.Sleep(50);
+                mutex.ReleaseMutex();
+                DataGridViewInformacion.Rows.Clear();
+            }
+        }
+        private void CargarHistorial()
+        {
+            for (int i = 0; i < Contenedor.ListaHistorial.Count(); i++)
+            {
+                DataGridViewInformacion.Rows.Add(Contenedor.ListaHistorial[i].Fecha, Contenedor.ListaHistorial[i].DirecionWEB);
+            }
+        }
+    }
+}
---
+++ Navegador WEB/Forms/Ventana.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using NavegadorWEB.Forms;
+using NavegadorWEB.Clases;
+
+namespace NavegadorWEB.Forms
+{
+    public partial class Ventana : Form
+    {
+        public Ventana()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        public Thread HiloPestanna
+        {
+            get;
+            set;
+        }
+
+        private void ToolStripButtonAtras_Click(object sender, EventArgs e)
+        {
+            WebBrowserPrincipal.GoBack();
+        }
+
+        private void ToolStripButtonAdelante_Click(object sender, EventArgs e)
+        {
+            WebBrowserPrincipal.GoForward();
+        }
+
+        private void ToolStripButtonRecargar_Click(object sender, EventArgs e)
+        {
+            WebBrowserPrincipal.Refresh();
+        }
+
+        private void ToolStripButtonBuscar_Click(object sender, EventArgs e)
+        {
+            if (ToolStripTextBoxURL.TextLength > 0)
+            {
+                try
+                {
+                    WebBrowserPrincipal.Navigate(ToolStripTextBoxURL.Text);
+                    Mutex mutex = new Mutex();
+                    mutex.WaitOne();
+                    Thread.Sleep(50);
+                    Contenedor.Busqueda = ToolStripTextBoxURL.Text;
+                    Contenedor.BusquedaEcha = true;
+                    Thread.Sleep(50);
+                    mutex.ReleaseMutex();
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
+
+        private void ToolStripButtonNuevaPestanna_Click(object sender, EventArgs e)
+        {
+            Mutex mutex = new Mutex();
+            mutex.WaitOne();
+            Thread.Sleep(50);
+            Contenedor.AbrirPestanna = true;
+            Thread.Sleep(50);
+            mutex.ReleaseMutex();
+        }
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            //Esta linea permite ejecutar varios hilos diferentes al hilo principal
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private void ToolStripButtonCerrar_Click(object sender, EventArgs e)
+        {
+            Mutex mutex = new Mutex();
+            mutex.WaitOne();
+            Thread.Sleep(50);
+            Contenedor.CerrarPestanna = true;
+            HiloPestanna.Abort();
+            Thread.Sleep(50);
+            mutex.ReleaseMutex();
+        }
+
+        private void ToolStripMenuItemHistorial_Click(object sender, EventArgs e)
+        {
+            Mutex mutex = new Mutex();
+            mutex.WaitOne();
+            Thread.Sleep(50);
+            Contenedor.AbrirHistorial = true;
+            Thread.Sleep(50);
+            mutex.ReleaseMutex();
+        }
+
+        private void ToolStripMenuItemCache_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("¿Desea borrar el cache de la aplicación?", "Confirmacion", MessageBoxButtons.YesNoCancel);
+            if (resultado == DialogResult.Yes)
+            {
+
+            }
+        }
+    }
+}
